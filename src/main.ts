05ca20fb603530af2e25772b74bf0b49fa789fe2
yakoltsevich <tpaktap1221@gmail.com>
2025-05-18 13:07:44 +0200

    в

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(cookieParser()); // ✅ ← вот это ключ
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin:  process.env.FRONTEND_URL ?? 'http://localhost:3001',
     credentials: true,
   });
 
