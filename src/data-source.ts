6d65537a258e5e84f3896ccd5bc2b8a1621cdb21
yakoltsevich <tpaktap1221@gmail.com>
2025-06-18 14:22:36 +0200

    qweфыв

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,20 +1,13 @@
 import { DataSource } from 'typeorm';
-import { config } from 'dotenv';
-import { User } from './users/entities/user.entity';
-import { UserSettings } from './user-settings/entities/user-settings.entity';
-import { Menu } from './menu/entities/menu.entity';
-import { Grocery } from './groceries/entities/grocery.entity';
-import { MenuDay } from './menu/entities/menu-day.entity';
-
-config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  synchronize: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: true,
   logging: true,
-  ssl:
-    process.env.DATABASE_SSL === 'true' ? { rejectUnauthorized: false } : false,
-  entities: [User, UserSettings, Menu, Grocery, MenuDay],
-  migrations: ['src/migrations/*.ts'],
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
 });
